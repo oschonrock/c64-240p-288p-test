27b74e9b18f37f47c483f6ef52b00527e5f12774
Oliver Schonrock <oliver@schonrocks.com>
2025-07-05 17:45:23 +0100

    allow color change, and show/help completely

--- test240p.cpp
+++ test240p.cpp
@@ -33,8 +33,8 @@ static const char* help_text[2][14] = {
         SCRC("flicker test"),
         "",
         SCRC("you should see a"),
-        SCRC("flickering black"),
-        SCRC("shape. it should not"),
+        SCRC("flickering shape."),
+        SCRC("it should not"),
         SCRC("be striped."),
         "",
         SCRC("move it with joyst."),
@@ -49,8 +49,8 @@ static const char* help_text[2][14] = {
         SCRC("striped sprite test"),
         "",
         SCRC("you should see a"),
-        SCRC("solid black shape"),
-        SCRC("on stripey bg."),
+        SCRC("solid shape on a "),
+        SCRC("stripey background."),
         "",
         SCRC("move it with joyst."),
         SCRC("will appear stripey"),
@@ -63,7 +63,15 @@ static const char* help_text[2][14] = {
     },
 };
 
+static const char bgch = 96; // shift space
+
 void show_help(char mode) {
+  dprint(9, 20, SCRC("joystick port 2 - move shape"), 40);
+  dprint(9, 21, SCRC("1..8 - change shape color"), 40);
+  dprint(9, 22, SCRC("f1 - show / hide help"), 40);
+  dprint(9, 23, SCRC("f3 - drop shadow / flicker test"), 40);
+  dprint(9, 24, SCRC("f5 - striped sprite test"), 40);
+
   for (char i = 0; i < ARRAYSIZE(help_text[mode]); i++) {
     dprint(20, i, help_text[mode][i], 40);
   };
@@ -86,8 +94,6 @@ void update_sprites(unsigned xpos, char ypos, bool shown) {
   }
 }
 
-static const char bgch = 96; // shift space
-
 void set_bg_char_even_rows(char rowval) {
   for (char i = 0; i < 4; i++) charset[bgch * 8 + (i * 2)] = rowval;
 }
@@ -102,7 +108,6 @@ int main() {
   __asm { cli }
 
   memset(screen, bgch, 1000);
-  memset(color, VCOL_BLACK, 1000);
 
   vic.color_border = VCOL_LT_GREY;
   vic.color_back   = VCOL_LT_GREY;
@@ -110,48 +115,92 @@ int main() {
   spr_init(screen);
   vic.spr_multi = vic.spr_expand_x = vic.spr_expand_y = 0;
 
-  dprint(9, 22, SCRC("joystick port 2 - move shape"), 40);
-  dprint(9, 23, SCRC("f1 - drop shadow / flicker test"), 40);
-  dprint(9, 24, SCRC("f3 - striped sprite test"), 40);
-
-  unsigned xpos = 80;
-  char     ypos = 101;
-  bool shown   = true;
-  bool flicker = true;
-  show_help(0);
-  set_spr_image(0, VCOL_BLACK);
+  unsigned xpos             = 80;
+  char     ypos             = 101;
+  bool     sprite_shown     = true;
+  bool     help_shown       = true;
+443  char     mode             = 0;
+  char     clr              = 0;
+  bool     flicker          = true;
+  bool     update_help      = true;
+  bool     update_spr_image = true;
 
   while (true) {
     vic_waitFrame();
-    
     joy_poll(0);
     xpos += joyx[0];
     ypos += joyy[0];
-    update_sprites(xpos, ypos, shown);
-    if (flicker) shown = !shown;
-    
+    update_sprites(xpos, ypos, sprite_shown);
+    if (flicker) sprite_shown = !sprite_shown;
+
     keyb_poll();
     if (keyb_key & KSCAN_QUAL_DOWN) {
       switch (keyb_key & 0x7f) {
       case KSCAN_F1:
-        set_bg_char_even_rows(0);     // empty bg
-        set_spr_image(0, VCOL_BLACK); // use solid sprite
-        flicker = true;
-        show_help(0);
-        xpos = 80;
-        ypos = 101;
+        help_shown  = !help_shown;
+        update_help = true;
         break;
       case KSCAN_F3:
-        set_bg_char_even_rows(255);   // stripey bg
-        set_spr_image(1, VCOL_BLACK); // use stripey sprite
-        shown   = true;
-        flicker = false;
-        show_help(1);
-        xpos = 80;
-        ypos = 101;
+        set_bg_char_even_rows(0); // empty bg
+        flicker          = true;
+        mode             = 0;
+        update_help      = true;
+        update_spr_image = true;
+        xpos             = 80;
+        ypos             = 101;
+        break;
+      case KSCAN_F5:
+        set_bg_char_even_rows(255); // stripey bg
+        sprite_shown     = true;
+        flicker          = false;
+        mode             = 1;
+        update_help      = true;
+        update_spr_image = true;
+        xpos             = 80;
+        ypos             = 101;
+        break;
+      default:
+        char ch = keyb_codes[keyb_key & 0x7f];
+        if ('1' <= ch && ch <= '8') {
+          clr              = ch - '1';
+          update_spr_image = true;
+        }
         break;
       }
     }
+    if (update_help) {
+      update_spr_image = true;
+      if (help_shown) {
+        show_help(mode);
+      } else {
+#pragma unroll(page)
+        for (unsigned i = 0; i < 1000; i++) {
+          screen[i] = bgch;
+        }
+      }
+      update_help = false;
+    }
+    if (update_spr_image) {
+      set_spr_image(mode, clr);
+#pragma unroll(page)
+      for (unsigned i = 0; i < 1000; i++) {
+        color[i] = clr;
+      }
+      if (help_shown) {
+        for (char y = 20; y < 25; y++) {
+          for (char x = 9; x < 40; x++) {
+            (color + y * 40)[x] = VCOL_BLACK;
+          }
+        }
+        for (char y = 0; y < 14; y++) {
+          for (char x = 20; x < 40; x++) {
+            (color + y * 40)[x] = VCOL_BLACK;
+          }
+        }
+      }
+      update_spr_image = false;
+    }
   }
+
   return 0;
 }
